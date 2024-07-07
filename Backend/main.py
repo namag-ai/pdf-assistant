db925bd4ef16fb336d9a9e8532ec0ca056fd4e36
namag-ai <naormagnagi@gmail.com>
2024-07-07 19:18:22 +0000

    changes

--- Backend/main.py
+++ Backend/main.py
@@ -43,4 +43,9 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
     return text
 
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
 
