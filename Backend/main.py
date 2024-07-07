c6e06660c2fb00f28a8dcd5e64b79d1c1abbbff6
namag-ai <naormagnagi@gmail.com>
2024-07-07 19:12:13 +0000

    changes

--- Backend/main.py
+++ Backend/main.py
@@ -20,3 +20,12 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
