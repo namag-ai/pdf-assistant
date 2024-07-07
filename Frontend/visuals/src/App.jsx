19484cfa5ec68f1a2bd707434e433a74442c4e98
namag-ai <naormagnagi@gmail.com>
2024-07-07 20:53:28 +0000

    changes

--- Frontend/visuals/src/App.jsx
+++ Frontend/visuals/src/App.jsx
@@ -3,6 +3,10 @@ import axios from 'axios';
 
 
 function App() {
+
+  const [files, setFiles] = useState([]);
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
   
 
   return (
