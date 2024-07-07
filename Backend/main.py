31c81fc8b1278d165df1a9959aaa79c149629e54
namag-ai <naormagnagi@gmail.com>
2024-07-07 19:17:45 +0000

    changes

--- Backend/main.py
+++ Backend/main.py
@@ -33,4 +33,14 @@ app.add_middleware(
 
 class QuestionRequest(BaseModel):
     question: str
+    
+    
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
 
