c5b89d202ed5e9273fc2f838323696c6b2a15460
namag-ai <naormagnagi@gmail.com>
2024-07-07 19:08:13 +0000

    changes

--- Backend/main.py
+++ Backend/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+
 from PyPDF2 import PdfReader
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
+
 from dotenv import load_dotenv
 import os
