3b45a3b34fee0a2fc6cd5fd7f4fce08ca544b891
namag-ai <naormagnagi@gmail.com>
2024-07-08 11:02:02 +0000

    changes

--- Frontend/visuals/src/App.jsx
+++ Frontend/visuals/src/App.jsx
@@ -91,7 +91,7 @@ function App() {
           onClick={handleUpload}
           className={`bg-blue-500 hover:bg-blue-600
              text-white px-4 py-2 rounded-md w-full ${uploading ? 'opacity-50 cursor-not-allowed' : ''}`}
-          disabled={!uploaded || uploading}
+          disabled={uploading}
         >
           {uploading ? 'Uploading...' : 'Upload Files'}
         </button>
